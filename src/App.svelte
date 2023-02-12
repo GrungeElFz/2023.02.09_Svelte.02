b32beb0780e7afbbe4f597c82f2a0607c968f1e5
GrungeElFz <elf0114_@hotmail.com>
2023-02-12 11:13:34 -0600

    Initialized:    Installed uuid dependency (package.json) Modified:   Imported uuid v.4 (src/App.svelte)             Created a dummy array 'toDolists'             Passed a 'toDoLists' as a prop to <ToDoList> component Modified:   Exported an exmpty array 'toDolists' (src/lib/ToDoList.svelte)             Created an <ul> element             Added an each statement in the element             Defined an id for each item             Defined a variable const number

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,27 @@
 <script>
   import ToDoList from "./lib/ToDoList.svelte";
+  import { v4 as uuid } from 'uuid';
+
+  const toDoLists = [
+    {
+      id: uuid(),
+      title: 'First task',
+      completed: true
+    },
+    {
+      id: uuid(),
+      title: 'Second task',
+      completed: false
+    },
+    {
+      id: uuid(),
+      title: 'Third task',
+      completed: false
+    }
+  ];
 </script>
 
-<ToDoList />
+<ToDoList {toDoLists} />
 
 <style>
 
--- src/lib/ToDoList.svelte
+++ src/lib/ToDoList.svelte
@@ -1,1 +1,10 @@
-Test
+<script>
+  export let toDoLists = [];
+</script>
+
+<ul>
+  {#each toDoLists as toDoList, index (toDoList.id) }
+    {@const number = index + 1}
+    <li>{number}. {toDoList.title}</li>
+  {/each}
+</ul>
