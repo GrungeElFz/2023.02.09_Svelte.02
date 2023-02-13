088b44a9b330a58c923b59add4203b8f8e4ab078
GrungeElFz <elf0114_@hotmail.com>
2023-02-13 16:01:51 -0600

    Modified:   Added a nonfunctional input form and button (src/lib/ToDoList.svelte)

--- src/lib/ToDoList.svelte
+++ src/lib/ToDoList.svelte
@@ -1,10 +1,17 @@
 <script>
-  export let toDoLists = [];
+	export let toDoLists = [];
 </script>
 
-<ul>
-  {#each toDoLists as { id, title }, index (id) }
-    {@const number = index + 1}
-    <li>{number}. {title}</li>
-  {/each}
-</ul>
+<div class="toDoLists-wrapper">
+	<ul>
+		{#each toDoLists as { id, title }, index (id)}
+			{@const number = index + 1}
+			<li>{number}. {title}</li>
+		{/each}
+	</ul>
+
+	<form class="toDoLists-form">
+		<input />
+		<button type="submit">Add</button>
+	</form>
+</div>
