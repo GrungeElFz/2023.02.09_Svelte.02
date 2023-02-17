47e54c7e68045331a60e997b9800c99677d86bab
GrungeElFz <elf0114_@hotmail.com>
2023-02-16 23:59:57 -0600

    Formatted:  src/App.svelte , app.css , main.js

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,28 @@
 <script>
-  import ToDoList from "./lib/ToDoList.svelte";
-  import { v4 as uuid } from 'uuid';
+	import ToDoList from './lib/ToDoList.svelte';
+	import { v4 as uuid } from 'uuid';
 
-  const toDoLists = [
-    {
-      id: uuid(),
-      title: 'First task',
-      completed: true
-    },
-    {
-      id: uuid(),
-      title: 'Second task',
-      completed: false
-    },
-    {
-      id: uuid(),
-      title: 'Third task',
-      completed: false
-    }
-  ];
+	const toDoLists = [
+		{
+			id: uuid(),
+			title: 'First task',
+			completed: true
+		},
+		{
+			id: uuid(),
+			title: 'Second task',
+			completed: false
+		},
+		{
+			id: uuid(),
+			title: 'Third task',
+			completed: false
+		}
+	];
 </script>
 
 <h2>{toDoLists.length} Tasks</h2>
 <ToDoList {toDoLists} />
 
 <style>
-
 </style>
